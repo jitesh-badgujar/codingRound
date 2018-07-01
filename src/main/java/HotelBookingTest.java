d93449112893891798e2d1038ed85a3b820694ca
Jitesh Badgujar <jitesh.badgujar@gmail.com>
2018-07-01 00:15:00 -0300

    Fixed HotelBookingTest using pageFactory initElements

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,38 +1,27 @@
+import java.util.concurrent.TimeUnit;
+
 import com.sun.javafx.PlatformUtil;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class HotelBookingTest {
 
     WebDriver driver;
-
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
-
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
-
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
-
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
-
+    
     @Test
     public void shouldBeAbleToSearchForHotels() {
         setDriverPath();
         driver = new ChromeDriver();
         driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
-
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        HotelBookingPage hotelBookingPage = PageFactory.initElements(driver, HotelBookingPage.class);
+        hotelBookingPage.searchHotels();
 
         driver.quit();
 
